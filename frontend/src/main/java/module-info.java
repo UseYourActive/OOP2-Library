c6966eef3720c58e04d3fd4b2f5e363cc15f81b5
UseYourActive <alexorozov@gmail.com>
2023-10-11 02:08:41 +0300

    added the lombok dependency in module-info.java

--- frontend/src/main/java/module-info.java
+++ frontend/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.library.oop2library {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires lombok;
 
     opens com.library.oop2library to javafx.fxml;
     exports com.library.oop2library;
