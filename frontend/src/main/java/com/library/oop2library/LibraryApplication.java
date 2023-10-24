b5e66a3b527d054bba4d2ca3389925ea20ded7e8
BogomilDonkov <bogson223@gmail.com>
2023-10-24 09:27:12 +0300

    Frontend

---
+++ frontend/src/main/java/com/library/oop2library/LibraryApplication.java
@@ -0,0 +1,24 @@
+package com.library.oop2library;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LibraryApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(LibraryApplication.class.getResource("AccessPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setResizable(false);
+        stage.setTitle("Library");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
