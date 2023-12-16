ece2489c029002bf8ee25eddf065ffcf981690ed
UseYourActive <alexorozov@gmail.com>
2023-12-16 20:24:25 +0200

    edited the ServiceFactory class.

--- src/main/java/com/library/backend/services/ServiceFactory.java
+++ src/main/java/com/library/backend/services/ServiceFactory.java
@@ -6,11 +6,9 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 public class ServiceFactory {
-
-
         @Getter
         @RequiredArgsConstructor
-        private enum OperationProcessorType {
+        private enum ServiceType {
             ADMIN_SERVICE(AdminService.class),
             ACCESS_SERVICE(AccessService.class),
             REGISTER_SERVICE(RegisterService.class);
@@ -18,31 +16,24 @@ public class ServiceFactory {
             private final Class<? extends Service> serviceClass;
         }
 
-
         public static Service getService(Class<? extends Service> serviceClass) {
-
             Service service;
 
-            switch (getOperationProcessorType(serviceClass)) {
+            switch (getServiceType(serviceClass)) {
                 case ADMIN_SERVICE -> service = serviceClass.cast(new AdminService(new BookRepository(),new UserRepository()));
-
                 case ACCESS_SERVICE -> service = serviceClass.cast(new AccessService(new UserRepository()));
-
                 case REGISTER_SERVICE -> service= serviceClass.cast(new RegisterService(new UserRepository()));
-
                 default -> throw new RuntimeException("There is no such enum");
             }
 
             return service;
         }
 
+        private static ServiceType getServiceType(Class<? extends Service> serviceClass) {
+            ServiceType processorType = null;
 
-        private static OperationProcessorType getOperationProcessorType(Class<? extends Service> processorClass) {
-
-            OperationProcessorType processorType = null;
-
-            for (OperationProcessorType type : OperationProcessorType.values()) {
-                if (type.serviceClass == processorClass) {
+            for (ServiceType type : ServiceType.values()) {
+                if (type.serviceClass == serviceClass) {
                     processorType = type;
                     break;
                 }
@@ -50,5 +41,4 @@ public class ServiceFactory {
 
             return processorType;
         }
-
 }
