9fc931c7024db6abc83c9a9b467df60d09c6893d
UseYourActive <alexorozov@gmail.com>
2023-12-21 20:23:20 +0200

    Small fix: Removed the usage of a reprecated method getMarker()

--- src/main/java/com/library/frontend/utils/colors/CustomHighlightingMarker.java
+++ src/main/java/com/library/frontend/utils/colors/CustomHighlightingMarker.java
@@ -6,10 +6,12 @@ import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 
+import java.util.List;
+
 public class CustomHighlightingMarker extends ForegroundCompositeConverterBase<ILoggingEvent> {
     @Override
     protected String getForegroundColorCode(ILoggingEvent event) {
-        Marker marker = event.getMarker();
-        return marker != null && marker.contains(MarkerFactory.getMarker("highlight")) ? ANSIConstants.YELLOW_FG : ANSIConstants.DEFAULT_FG;
+        List<Marker> markerList = event.getMarkerList();
+        return markerList != null && markerList.contains(MarkerFactory.getMarker("highlight")) ? ANSIConstants.YELLOW_FG : ANSIConstants.DEFAULT_FG;
     }
 }
