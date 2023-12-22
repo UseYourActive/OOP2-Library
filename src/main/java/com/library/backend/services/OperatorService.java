8ac6b7c3a922c7f03f211976701e07b9b0a8db6d
BogomilDonkov <bogson223@gmail.com>
2023-12-22 08:45:32 +0200

    -Added Logger to OperatorService.java

--- src/main/java/com/library/backend/services/OperatorService.java
+++ src/main/java/com/library/backend/services/OperatorService.java
@@ -1,4 +1,8 @@
 package com.library.backend.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class OperatorService implements Service{
+    private final Logger logger = LoggerFactory.getLogger(AdminService.class);
 }
