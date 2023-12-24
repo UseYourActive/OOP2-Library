d385b983bf10dbc097c695b35939270ed39db84b
UseYourActive <alexorozov@gmail.com>
2023-12-24 13:23:05 +0200

    Update on the controller for reader creation.

--- src/main/java/com/library/frontend/controllers/operator/CreateReaderProfileController.java
+++ src/main/java/com/library/frontend/controllers/operator/CreateReaderProfileController.java
@@ -1,5 +1,10 @@
 package com.library.frontend.controllers.operator;
 
+import com.google.common.collect.Lists;
+import com.library.backend.services.OperatorService;
+import com.library.backend.services.ServiceFactory;
+import com.library.database.entities.Reader;
+import com.library.database.enums.Rating;
 import com.library.frontend.controllers.base.Controller;
 import com.library.frontend.utils.SceneLoader;
 import javafx.fxml.FXML;
@@ -8,6 +13,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class CreateReaderProfileController implements Controller {
@@ -17,16 +23,32 @@ public class CreateReaderProfileController implements Controller {
     @FXML public TextField phoneNumberTextField;
     @FXML public TextField emailTextField;
     @FXML public Button createReaderProfileButton;
-
     @FXML public Button cancelButton;
+    private OperatorService operatorService;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        operatorService = (OperatorService) ServiceFactory.getService(OperatorService.class);
     }
     @FXML
     public void createReaderProfileButtonOnMouseClicked(MouseEvent mouseEvent) {
+        String firstName = firstNameTextField.getText();
+        String middleName = middleNameTextField.getText();
+        String lastName = lastNameTextField.getText();
+        String phoneNumber = phoneNumberTextField.getText();
+        String email = emailTextField.getText();
 
+        Reader reader = Reader.builder()
+                .firstName(firstName)
+                .middleName(middleName)
+                .lastName(lastName)
+                .email(email)
+                .phoneNumber(phoneNumber)
+                .bookForms(Lists.newArrayList())
+                .rating(Rating.THREE_STAR)
+                .build();
+
+        operatorService.createReader(reader);
     }
     @FXML
     public void cancelButtonOnMouseClicked(MouseEvent mouseEvent) {
