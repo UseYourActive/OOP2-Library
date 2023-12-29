d9c181f4746c94d13ecbc3259344f2d383472ccf
UseYourActive <alexorozov@gmail.com>
2023-12-29 04:46:47 +0200

    Added Mockito as a dependency for testing. Made some Tests to see what they are like

---
+++ src/test/java/LogInServiceTest.java
@@ -0,0 +1,179 @@
+import com.library.backend.exception.UserNotFoundException;
+import com.library.backend.services.LogInService;
+import com.library.database.entities.User;
+import com.library.database.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class LogInServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private LogInService logInService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testGetUser() {
+        // Arrange
+        User testUser = new User();
+        testUser.setUsername("testUser");
+
+        // Mocking the behavior of UserRepository
+        when(userRepository.findByUsername("testUser")).thenReturn(Optional.of(testUser));
+
+        // Act
+        User result = logInService.getUser(testUser);
+
+        // Assert
+        assertEquals(testUser, result);
+        verify(userRepository).findByUsername("testUser");
+    }
+
+    @Test
+    public void testGetUserUserNotFound() {
+        // Arrange
+        User testUser = new User();
+        testUser.setUsername("nonExistentUser");
+
+        // Mocking the behavior of UserRepository
+        when(userRepository.findByUsername("nonExistentUser")).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> logInService.getUser(testUser));
+        verify(userRepository).findByUsername("nonExistentUser");
+    }
+
+    @Test
+    public void testGetUserWithNullUsername() {
+        // Arrange
+        User testUser = new User();
+        testUser.setUsername(null);
+
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> logInService.getUser(testUser));
+        verify(userRepository).findByUsername(isNull());
+    }
+
+
+    @Test
+    public void testGetUserWithEmptyUsername() {
+        // Arrange
+        User testUser = new User();
+        testUser.setUsername("");
+
+        // Mocking the behavior of UserRepository
+        when(userRepository.findByUsername("")).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> logInService.getUser(testUser));
+        verify(userRepository).findByUsername("");
+    }
+
+    @Test
+    public void testGetUserWithWhitespaceUsername() {
+        // Arrange
+        User testUser = new User();
+        testUser.setUsername("   ");
+
+        // Mocking the behavior of UserRepository
+        when(userRepository.findByUsername("   ")).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> logInService.getUser(testUser));
+        verify(userRepository).findByUsername("   ");
+    }
+
+    @Test
+    public void testGetUserWithExistingUsername() {
+        // Arrange
+        User testUser = new User();
+        testUser.setUsername("existingUser");
+
+        // Mocking behavior
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.of(testUser));
+
+        // Act
+        User resultUser = logInService.getUser(testUser);
+
+        // Assert
+        assertEquals(testUser, resultUser);
+        verify(userRepository).findByUsername("existingUser");
+    }
+
+    @Test
+    public void testGetUserWithNonexistentUsername() {
+        // Arrange
+        User testUser = new User();
+        testUser.setUsername("nonexistentUser");
+
+        // Mocking behavior
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> logInService.getUser(testUser));
+        verify(userRepository).findByUsername("nonexistentUser");
+    }
+
+    @Test
+    public void testGetUserWithValidCredentials() {
+        // Arrange
+        User testUser = new User();
+        testUser.setUsername("validUser");
+        testUser.setPassword("validPassword");
+
+        // Mocking behavior
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.of(testUser));
+
+        // Act
+        User resultUser = logInService.getUser(testUser);
+
+        // Assert
+        assertEquals(testUser, resultUser);
+        verify(userRepository).findByUsername("validUser");
+    }
+
+    @Test
+    public void testGetUserWithInvalidCredentials() {
+        // Arrange
+        User testUser = new User();
+        testUser.setUsername("validUser");
+        testUser.setPassword("invalidPassword");
+
+        // Mocking behavior
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> logInService.getUser(testUser));
+        verify(userRepository).findByUsername("validUser");
+    }
+
+    @Test
+    public void testGetUserWithNullUser() {
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> logInService.getUser(null));
+        verify(userRepository, never()).findByUsername(anyString());
+    }
+
+    @Test
+    public void testGetUserWithNullUserAndNullPassword() {
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> logInService.getUser(null));
+        verify(userRepository, never()).findByUsername(anyString());
+    }
+}
---
+++ src/test/java/UserRepositoryTest.java
@@ -0,0 +1,172 @@
+import com.library.database.entities.User;
+import com.library.database.repositories.UserRepository;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+class UserRepositoryTest {
+
+    @Mock
+    private Session session;
+
+    @InjectMocks
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void findById_UserFound_ReturnsUserOptional() {
+        // Arrange
+        Long userId = 1L;
+        User expectedUser = new User();
+        expectedUser.setId(userId);
+
+        when(session.get(User.class, userId)).thenReturn(expectedUser);
+
+        // Act
+        Optional<User> result = userRepository.findById(userId);
+
+        // Assert
+        assertTrue(result.isPresent());
+        assertEquals(expectedUser, result.get());
+        verify(session).get(User.class, userId);
+    }
+
+    @Test
+    void findById_UserNotFound_ReturnsEmptyOptional() {
+        // Arrange
+        Long userId = 1L;
+
+        when(session.get(User.class, userId)).thenReturn(null);
+
+        // Act
+        Optional<User> result = userRepository.findById(userId);
+
+        // Assert
+        assertTrue(result.isEmpty());
+        verify(session).get(User.class, userId);
+    }
+
+    @Test
+    void findAll_MultipleUsers_ReturnsListOfUsers() {
+        // Arrange
+        User user1 = new User();
+        User user2 = new User();
+        List<User> expectedUsers = Arrays.asList(user1, user2);
+
+        Query<User> query = mock(Query.class);
+        when(session.createQuery(eq("SELECT u FROM User u"), eq(User.class))).thenReturn(query);
+        when(query.getResultList()).thenReturn(expectedUsers);
+
+        // Act
+        List<User> result = userRepository.findAll();
+
+        // Assert
+        assertEquals(expectedUsers.size(), result.size());
+        assertTrue(result.containsAll(expectedUsers));
+        verify(session).createQuery(eq("SELECT u FROM User u"), eq(User.class));
+    }
+
+    @Test
+    void findAll_NoUsers_ReturnsEmptyList() {
+        // Arrange
+        List<User> expectedUsers = Arrays.asList();
+
+        Query<User> query = mock(Query.class);
+        when(session.createQuery(eq("SELECT u FROM User u"), eq(User.class))).thenReturn(query);
+        when(query.getResultList()).thenReturn(expectedUsers);
+
+        // Act
+        List<User> result = userRepository.findAll();
+
+        // Assert
+        assertTrue(result.isEmpty());
+        verify(session).createQuery(eq("SELECT u FROM User u"), eq(User.class));
+    }
+
+    @Test
+    void getById_UserFound_ReturnsUser() {
+        // Arrange
+        Long userId = 1L;
+        User expectedUser = new User();
+        expectedUser.setId(userId);
+
+        when(session.get(User.class, userId)).thenReturn(expectedUser);
+
+        // Act
+        User result = userRepository.getById(userId);
+
+        // Assert
+        assertEquals(expectedUser, result);
+        verify(session).get(User.class, userId);
+    }
+
+    @Test
+    void getById_UserNotFound_ReturnsNull() {
+        // Arrange
+        Long userId = 1L;
+
+        when(session.get(User.class, userId)).thenReturn(null);
+
+        // Act
+        User result = userRepository.getById(userId);
+
+        // Assert
+        assertNull(result);
+        verify(session).get(User.class, userId);
+    }
+
+    @Test
+    void findByUsername_UserFound_ReturnsUserOptional() {
+        // Arrange
+        String username = "testUser";
+        User expectedUser = new User();
+        expectedUser.setUsername(username);
+
+        Query<User> query = mock(Query.class);
+        when(session.createQuery(anyString(), eq(User.class))).thenReturn(query);
+        when(query.setParameter(anyString(), any())).thenReturn(query);
+        when(query.uniqueResult()).thenReturn(expectedUser);
+
+        // Act
+        Optional<User> result = userRepository.findByUsername(username);
+
+        // Assert
+        assertTrue(result.isPresent());
+        assertEquals(expectedUser, result.get());
+        verify(session).createQuery(anyString(), eq(User.class));
+    }
+
+    @Test
+    void findByUsername_UserNotFound_ReturnsEmptyOptional() {
+        // Arrange
+        String username = "nonExistentUser";
+
+        Query<User> query = mock(Query.class);
+        when(session.createQuery(anyString(), eq(User.class))).thenReturn(query);
+        when(query.setParameter(anyString(), any())).thenReturn(query);
+        when(query.uniqueResult()).thenReturn(null);
+
+        // Act
+        Optional<User> result = userRepository.findByUsername(username);
+
+        // Assert
+        assertTrue(result.isEmpty());
+        verify(session).createQuery(anyString(), eq(User.class));
+    }
+}
